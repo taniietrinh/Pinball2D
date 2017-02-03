cb4cdeb63040d3e725c94cca9ac7bc8c5f2e407d
Thanh Trinh <taniatrinh@2c-be-8-e6-ef-1e.wlan.lsu.edu>
2017-02-03 11:14:59 -0600

    Create bumpers

---
+++ Assets/LeftFlip.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LeftFlip : MonoBehaviour {
+	Vector2 initialPos;
+
+	// Use this for initialization
+	void Start () {
+		initialPos = gameObject.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.LeftShift)) {
+			gameObject.transform.Rotate (0, 0, 40);
+		}
+		if (Input.GetKeyUp (KeyCode.LeftShift)) {
+			gameObject.transform.position = initialPos;
+		}
+	}
+}
