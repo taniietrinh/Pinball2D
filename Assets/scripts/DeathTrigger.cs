a693b243963a3e14ce0807c7ad196a152a058f15
Thanh Trinh <trinhtania@gmail.com>
2017-02-06 10:19:39 -0600

    Add tags, delete and remake score counter

--- Assets/scripts/DeathTrigger.cs
+++ Assets/scripts/DeathTrigger.cs
@@ -6,5 +6,5 @@ public class DeathTrigger : MonoBehaviour {
 
 	void OnTriggerEnter2D(Collider2D other) {
 		other.gameObject.transform.position = new Vector3(-2,2,0);
-		}
+	}
 }
---
+++ Assets/scripts/ScoreCounter.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreCounter : MonoBehaviour {
+
+	private int count;
+	public Text countText;
+
+	// Use this for initialization
+	void Start () {
+		count = 0;
+		SetCountText ();
+	}
+
+	void onTriggerEnter2D(Collider2D other) {
+		if (other.gameObject.CompareTag("stars")) {
+			count += 5;
+			SetCountText ();
+		}
+		if (other.gameObject.CompareTag ("smallerStars")) {
+			count += 10;
+			SetCountText ();
+		}
+
+	}
+
+	void SetCountText() {
+		countText.text = "Score: " + count.ToString ();
+	}
+}
